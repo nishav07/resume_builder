081b8113e4f172686005dd892fd0c50723aeb1d0
unknown <nishavkumar07@gmail.com>
2025-05-31 18:15:41 +0530

    added to github

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+// const path = require('path');
+// const multer = require('multer');
+// const { v4: uuidv4 } = require('uuid');
+// const methodOverride = require('method-override');
+
+app.use(express.urlencoded({ extended:true }));
+// app.use('/uploads', express.static('uploads'));
+// app.use(methodOverride('_method'));
+app.set("view engine" , "ejs");
+app.set("views" , path.join(__dirname , "views"));
+app.use(express.static(path.join(__dirname , "public")));
+
+app.listen(port, () => {
+    console.log(`app working fine at port ${port}`);
+})
+
+app.get("/" , (req,res) => {
+    res.send("hello world");
+})
+
